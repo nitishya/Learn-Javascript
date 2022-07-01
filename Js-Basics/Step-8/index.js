0a17509fdfd738820ebf8f20971283860dcdb472
Nitish Kumar Yadav <nitishya143@gmail.com>
2022-07-01 16:55:22 +0530

    Array
    Array ,length

---
+++ Js-Basics/Step-8/index.js
@@ -0,0 +1,5 @@
+let selectedColors = ['red','blue'];
+selectedColors[2]='green'; //can be changed during runtime
+selectedColors[3]=1; //can store different type in array
+console.log(selectedColors);
+console.log(selectedColors.length); //length tell the number of elements in array 
