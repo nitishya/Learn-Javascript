27de4debc3ce72d9740650fbc50bd9e138ed91b9
Nitish Kumar Yadav <nitishya143@gmail.com>
2022-07-01 10:12:05 +0530

    Primitives/Value Types
    1.String
    2.Number
    3.Boolean
    4.undefined
    5.Null

---
+++ Js-Basics/Step-5/index.js
@@ -0,0 +1,6 @@
+let name = 'Nitish'; //string literal
+let age = 20; //number literal
+let isApproved = false; //boolean literal
+let firstName = undefined; //undefined literal
+let lastName = null; //null literal  are need to set in future
+
