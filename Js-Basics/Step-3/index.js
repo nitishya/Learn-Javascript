30aa80b345132b26643dce9d21deafcb2300528c
Nitish Kumar Yadav <nitishya143@gmail.com>
2022-07-01 09:27:43 +0530

    Variables
    learn creating variables

---
+++ Js-Basics/Step-3/index.js
@@ -0,0 +1,13 @@
+let name = 'nitish';
+console.log(name);
+
+//rules 
+//name cannot be a reserved character
+//should be meaningfull
+//cannot start with a number like 1name
+//cannot contain space or hyphen(-)
+//are case sensitive
+
+let firstName = 'rahul';  //camelcasing
+
+
