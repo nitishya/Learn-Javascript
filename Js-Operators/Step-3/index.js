d3bb75a283987f78ce5006e8b4d9dd67051db93c
Nitish Kumar Yadav <nitishya143@gmail.com>
2022-07-02 21:06:00 +0530

    comparison and equality operators

---
+++ Js-Operators/Step-3/index.js
@@ -0,0 +1,19 @@
+let x = 1;
+
+//Relational
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+
+//Equality 
+console.log(x === 1);
+console.log(x !== 1);
+
+//Strict equality (Type + Value)
+console.log(1 === 1);
+console.log('1' === 1);
+
+//loose equality 
+console.log(1 == 1);
+console.log('1' == 1);
