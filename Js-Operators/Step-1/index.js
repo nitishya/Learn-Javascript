c35fa8e04154484d50112a65facf270b68c56722
Nitish Kumar Yadav <nitishya143@gmail.com>
2022-07-02 20:46:21 +0530

    Airthmetic Operations

---
+++ Js-Operators/Step-1/index.js
@@ -0,0 +1,19 @@
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);  
+console.log(x ** y); //x to the power of y
+
+//increment 
+console.log(++x);  //value increment first
+console.log(x++);  //value print first 
+console.log(x); 
+
+//decrement
+console.log(--x);  //value increment first
+console.log(x--);  //value print first 
+console.log(x); 
