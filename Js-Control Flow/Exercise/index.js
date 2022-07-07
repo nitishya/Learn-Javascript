1b03879178a84baad990a25c6e5f21435a861102
Nitish Kumar Yadav <nitishya143@gmail.com>
2022-07-07 10:26:58 +0530

    questions to practice on loops

--- Js-Control Flow/Exercise/index.js
+++ Js-Control Flow/Exercise/index.js
@@ -41,3 +41,23 @@ function fizzBuzz(input){
 }
 
 
+//check speed 
+checkSpeed(130)
+
+
+function checkSpeed(speed){
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint){
+       console.log('Ok');
+       return;
+   } 
+    else{
+        const points = Math.floor(speed -speedLimit) / kmPerPoint;
+        if (points >= 12)
+           console.log('License suspended');
+        else 
+           console.log('Points', points);   
+    }   
+}
