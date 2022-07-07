3d22ccb9ab11ecb91e990bcb7fab360dc5546b60
Nitish Kumar Yadav <nitishya143@gmail.com>
2022-07-07 10:59:44 +0530

    Update index.js

--- Js-Control Flow/Exercise/index.js
+++ Js-Control Flow/Exercise/index.js
@@ -61,3 +61,17 @@ function checkSpeed(speed){
            console.log('Points', points);   
     }   
 }
+
+//print stars
+showStars(5);
+
+function showStars(rows){
+   for (let row = 1; row <= rows; row++){
+      let pattern = '';
+      for (let i = 0; i < row; i++)
+      pattern += '*';
+   console.log(pattern);
+   }
+
+}
+
