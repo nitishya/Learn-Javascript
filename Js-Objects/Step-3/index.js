b214a364a3885b7fac0aa10f92a61723074ec90b
Nitish Kumar Yadav <nitishya143@gmail.com>
2022-07-08 23:15:14 +0530

    template literals

---
+++ Js-Objects/Step-3/index.js
@@ -0,0 +1,26 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+//javascript math object 
+
+//template literals =>used to format the sentance as they appear in the code 
+
+const another = `this is 
+template literals`;   
+
+//note ' and ` both are different,the symbol ` found below ~ symbol/escape
+
+
+//Dynamic template literals use 
+const name = 'John';
+const another = 
+`Hi ${name} ${4 + 5},
+
+thankyou for joining the github
+
+Regards,
+Rahul`;
+
+console.log(another);
+
+
+
