1dc11c3cbaf5c53829318679f14a5933469a0a45
Nitish Kumar Yadav <nitishya143@gmail.com>
2022-07-08 23:21:01 +0530

    Date in javascript

---
+++ Js-Objects/Step-4/index.js
@@ -0,0 +1,8 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+
+const now = new Date();
+const date1 = new Date('May 11 2022 09:00');
+const date2 = new Date(2022, 6, 11, 9);
+
+now.getDate;
